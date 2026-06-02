9a42add128544bd8cc67146c17e9c796dc9bb056
Chan Sze Kong <szekong@gmail.com>
2026-06-02 22:03:29 +0800

    completed testing of prometheus and cloudwatch agent

--- terraform/eks/default/dashboard.tf
+++ terraform/eks/default/dashboard.tf
@@ -17,13 +17,40 @@ resource "kubernetes_config_map" "grafana_datasource_cloudwatch" {
   ]
 }
 
+locals {
+  grafana_dashboard_dir = "${path.module}/../../../grafana/dashboards"
+
+  grafana_dashboard_shards = {
+    a = [
+      "dynamodb-cloudwatch.json",
+      "eks-cloudwatch.json",
+      "k8s_prom_15661_.json",
+      "node_exporter_1860_rev45.json",
+    ]
+    b = [
+      "classical-lb-cloudwatch.json",
+      "ec2-cloudwatch.json",
+      "elb-cloudwatch.json",
+      "eks-managed-nodegroup-starter-configmap.yaml",
+      "eks-managed-nodegroup-starter.json",
+      "k8-clustering-prometheus.json",
+      "logs-cloudwatch.json",
+      "loki_14055_rev5.json",
+      "mq-cloudwatch.json",
+      "rds-cloudwatch.json",
+    ]
+  }
+}
+
 resource "kubernetes_config_map_v1" "grafana_dashboards" {
+  for_each = local.grafana_dashboard_shards
+
   depends_on = [
     kubernetes_namespace_v1.monitoring
   ]
 
   metadata {
-    name      = "monitoring-grafana-dashboards"
+    name      = "monitoring-grafana-dashboards-${each.key}"
     namespace = kubernetes_namespace_v1.monitoring.metadata[0].name
     labels = {
       grafana_dashboard = "1"
@@ -31,7 +58,7 @@ resource "kubernetes_config_map_v1" "grafana_dashboards" {
   }
 
   data = {
-    for f in fileset("${path.module}/../../../grafana/dashboards", "*.json") :
-    f => file("${path.module}/../../../grafana/dashboards/${f}")
+    for f in each.value :
+    f => file("${local.grafana_dashboard_dir}/${f}")
   }
 }
